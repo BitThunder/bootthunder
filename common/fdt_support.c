b63c0bfe4b8f4159672278a51984b58aa0eb6640
Andreas Friedl <afriedl@riegl.com>
2013-07-09 15:24:59 +0200

    [BOOTM] Allow extending or override behaviour of fdt bootargs for bootm command.
    Signed-off-by: James Walmsley <james@fullfat-fs.co.uk>

--- common/fdt_support.c
+++ common/fdt_support.c
@@ -42,12 +42,39 @@ int fdt_chosen(void *fdt, int force) {
 		return 0;
 	}
 
+	BT_ENV_VARIABLE *pforce = BT_ShellGetEnv("bootargs_override");
+	if(pforce) {
+		force = 1;
+	}
+
+	BT_BOOL bAppend = BT_FALSE;
+	BT_ENV_VARIABLE *append = BT_ShellGetEnv("bootargs_extend");
+	if(append) {
+		bAppend = BT_TRUE;
+		force = 1;
+	}
+	
+
 	char *str = bootargs->o.string->s;
 	const char *path = fdt_getprop(fdt, nodeoffset, "bootargs", NULL);
 	if(!path || force) {
-		error = fdt_setprop(fdt, nodeoffset, "bootargs", str, bootargs->o.string->length + 1);
+		if(bAppend) {
+			printf("path = %s\r\n", path);
+			BT_u32 total_length = strlen(str);
+			total_length += strlen(path);
+			char *new_str = BT_kMalloc(total_length + 2);
+			strcpy(new_str, path);
+			strcat(new_str, " ");
+			strcat(new_str, str);
+			str = new_str;
+			printf("new_str = %s\r\n", new_str);
+		}
+		error = fdt_setprop(fdt, nodeoffset, "bootargs", str, strlen(str) + 1);
 		if(error < 0) {
 			printf("fdt_chosen: Could not set bootargs: %s\n", fdt_strerror(error));
+		}
+		if(bAppend) {
+			BT_kFree(str);
 		}
 	}
 
