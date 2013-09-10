5fd9dd69e9ba20c448c0a523e27aadeb2427ca28
James Walmsley <james@fullfat-fs.co.uk>
2013-09-10 11:01:34 +0200

    [MACH] ZYNQ - Updated startup code to be compatible with latest BitThunder.

--- arch/arm/mach/zynq/startup.s
+++ arch/arm/mach/zynq/startup.s
@@ -3,17 +3,18 @@
 .globl _bt_startup_boot
 _bt_startup_boot:
 	ldr	r0,=1
-	ldr	r1,=0xf8f00208
+	ldr	r1,=0xf8f00208		@ Start the global timer.
 	str	r0,[r1]
 	ldr	r0,=0xDEADBEEF
-	bl	ps7_init			@ Run the clock configurator, as exported from Xilinx HW generator.
-	bl 	ps7_post_config
-	b	_bt_start
+	@bl	ps7_init			@ Run the clock configurator, as exported from Xilinx HW generator.
+	@bl ps7_post_config
+	bx	lr
+	mov	r0, r0
 .globl _sbrk
 _sbrk:
 	mov	pc, lr
-
+/*
 .globl ps7_post_config
 .weak ps7_post_config
 ps7_post_config:
-	mov	pc, lr
+	mov	pc, lr*/
